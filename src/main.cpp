9995fca525128bc429b8fc9a1d352db66b7f7ead
root <root@AnonBox.(none)>
2014-05-19 23:48:07 -0700

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -969,7 +969,7 @@ int64 GetProofOfWorkReward(int nHeight, int64 nFees, uint256 prevHash)
 	}
 	else if(nHeight == 1)
 	{
-		nSubsidy = 20000 * COIN;	
+		nSubsidy = 11000 * COIN;	
 		return nSubsidy + nFees;
 	}
 	else if(nHeight < 4320)
